850d224ab2a76bc54cee12f71d22d8e41a82db15
Albert Berger <alberger@gmail.com>
2020-10-12 02:02:46 +0300

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Demo_tiny
-Tiny example C++ program using Transd
+This example program demonstrates the basics of working of a C++ program with the embedded
+Transd. The C++ program here creates a graph of a math function, repeatedly calling a
+procedure defined in an external Transd program. The graph is outputted into a
+PPM image file.
+
+### How to build
+
+#### Windows
+
+1. Place the files 'asm.td' and 'getshade.td' from the repository 'src' directory
+to some writable directory. (Or leave them in place if their current directory is
+writable.) Into this directory the output file of the program will be written.
+
+2. Edit the 'asm.td' file and specify the path to the 'getshade.td' file.
+
+3. Edit the 'main.cpp' file: locate the following line at the top of the file:
+
+`std::string workingDir = "Y:\\your\\working\\directory\\"`
+
+and change it by specifying your output directory (with trailing backslash).
+
+4. Build the solution and run the executable. Check the image in the output "graph.ppm"
+file. PPM image files can be opened e.g. with LibreOffice, GIMP, etc. Report of any
+issues here: https://github.com/transd-lang/Demo_tiny/issues
+
+#### Linux
+
+To be written...
---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include "transd.hpp"
+
+//TODO: specify your working directory where the Transd files are located and where the output file
+// will be placed
+std::string workingDir = "Y:\\your\\working\\directory\\"
+
+
+using namespace std;
+
+int width = 1000;
+int height = 1000;
+int sz = width * height * 3;
+
+
+int wmain( int argc, wchar_t* argv[] )
+{
+	wstring testFilePath =  U16(workingDir) + L"asm.td";
+
+	transd::HPROG prog = transd::createAssembly();
+
+	transd::loadProgram( prog, testFilePath );
+	transd::TDType* func = transd::getProc( prog, L"tst::getShade" );
+	wstring sx = L"x", sy = L"y";
+	double* x = (double*)transd::getExportVariable( prog, sx );
+	double* y = (double*)transd::getExportVariable( prog, sy );
+
+	vector<uint8_t> outbuf(sz);
+
+	for( int i = 0; i < width; ++i ) {
+		for( int k = 0; k < height; ++k ) {
+			*x = (double)i - width / 2;
+			*y = (double)k;
+
+			int res = *(int*)transd::execute( func );
+			uint8_t	shade = 255 * (1 - res);
+
+			int k1 = height - k - 1;
+			outbuf[3 * k1*width + 3 * i] = shade;
+			outbuf[3 * k1*width + 3 * i + 1] = shade;
+			outbuf[3 * k1*width + 3 * i + 2] = shade;
+		}
+	}
+
+	string outFileName = workingDir + "graph.ppm";
+	FILE* f = fopen( outFileName.c_str(), "wb" );
+	char buf[40];
+	sprintf( buf, "P6 %d %d 255\n", width, height );
+	fwrite( buf, strlen( buf ), 1, f );
+	unsigned char* p = (uint8_t*)&outbuf[0];
+	for( int i = 0; i <= sz - width * 3; i += width * 3 ) {
+		fwrite( p + i, width * 3, 1, f );
+	}
+	fclose( f );
+	return 0;
+}
